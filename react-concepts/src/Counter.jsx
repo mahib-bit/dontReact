6d3fd24cf850b19ecd0d8e69dda2cf2569933b76
mahib-bit <sadmanmahib@gmail.com>
2026-05-09 16:25:50 +0600

    Counter And Button

---
+++ react-concepts/src/Counter.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react"
+
+export default function Counter () {
+    const [count, setCount] = useState(0)
+
+    const counterStyle = {
+        border : '2px solid darkred',
+        padding : '10px',
+    }
+    return (
+        <div style={counterStyle}>
+            <h3>Counter: {count}</h3>
+            <button onClick={() => setCount(count + 1)}>Add</button>
+        </div>
+    )
+}
